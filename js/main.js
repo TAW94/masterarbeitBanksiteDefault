8ab6c0680e719aab98bf550a62f7244edde7b81d
TAW94 <Tim.Wohlhoefner@gmail.com>
2020-06-09 17:53:26 +0200

    Visuelle Änderungen: - carousel entfernt und für Übersichtlichkeit durch 3 Blocks übersetzt - Footer überarbeitet - Scroll Button in Cover eindeutiger gemacht

--- js/main.js
+++ js/main.js
@@ -375,4 +375,13 @@ $(document).ready(function() {
 });
 
 
+//scroll-animation im Hero Index
+// $(function() {
+//   $('a[href*=#]').on('click', function(e) {
+//     e.preventDefault();
+//     $('html, body').animate({ scrollTop: $($(this).attr('href')).offset().top}, 500, 'linear');
+//   });
+// });
+
+
 });
