e1979835da281dccdada03d9ee79bbee03a44df6
TAW94 <Tim.Wohlhoefner@gmail.com>
2020-06-29 10:54:01 +0200

    Änderungen: - Erklärungen WIP

--- js/main.js
+++ js/main.js
@@ -375,6 +375,42 @@ $(document).ready(function() {
 });
 
 
+// display dynamic content based on Link
+// Parse the URL parameter
+	function getParameterByName(name, url) {
+	    if (!url) url = window.location.href;
+	    name = name.replace(/[\[\]]/g, "\\$&");
+	    var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+	        results = regex.exec(url);
+	    if (!results) return null;
+	    if (!results[2]) return '';
+	    return decodeURIComponent(results[2].replace(/\+/g, " "));
+	}
+	// Give the parameter a variable name
+	var dynamicContent = getParameterByName('grp');
+
+	 $(document).ready(function() {
+
+		// Check if the URL parameter is apples
+		if (dynamicContent == '0-1-0') {
+			$('#0-1-0').show();
+		}
+		// Check if the URL parameter is oranges
+		else if (dynamicContent == '0-1-1') {
+			$('#0-1-1').show();
+		}
+		// Check if the URL parameter is bananas
+		else if (dynamicContent == '1-0-0') {
+			$('#1-0-0').show();
+		}
+		// Check if the URL parmeter is empty or not defined, display default content
+		else {
+			$('#default-content').show();
+		}
+	});
+
+
+
 //scroll-animation im Hero Index
 // $(function() {
 //   $('a[href*=#]').on('click', function(e) {
