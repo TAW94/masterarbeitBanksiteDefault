850a52c19d9d9c45c5dbd0d2bae040db2ced73c0
TAW94 <Tim.Wohlhoefner@gmail.com>
2020-07-03 15:58:43 +0200

    Änderungen: - local Storage eingebaut

---
+++ js/getDynamicContent.js
@@ -0,0 +1,48 @@
+$(document).ready(function() {
+
+  var dynamicContent = localStorage.getItem("dynamicContent");
+
+  // if(window.location.href === "https://taw94.github.io/masterarbeitBanksiteDefault/entscheidung.html/" || window.location.href === "file:///C:/Users/timwo/github/masterarbeitBanksiteDefault/entscheidung.html"){
+
+ // $('#erklärungssection-000').addClass('dynamicContent');
+ // $('#erklärungssection-010').addClass('dynamicContent');
+ // $('#erklärungssection-011').addClass('dynamicContent');
+ // $('#erklärungssection-100').addClass('dynamicContent');
+ // $('#erklärungssection-101').addClass('dynamicContent');
+ // $('#erklärungssection-110').addClass('dynamicContent');
+ // $('#erklärungssection-111').addClass('dynamicContent');
+
+  // var dynamicContent = localStorage.getItem("groupKey");
+
+ // Check if the URL parameter is apples
+ if (dynamicContent == "0-1-0") {
+   $('#erklärungssection-010').show();
+ }
+ // Check if the URL parameter is oranges
+ else if (dynamicContent == "0-1-1") {
+   $('#erklärungssection-011').show();
+ }
+ // Check if the URL parameter is bananas
+ else if (dynamicContent == "1-0-0") {
+   $('#erklärungssection-100').show();
+ }
+ else if (dynamicContent == "1-0-1") {
+   $('#erklärungssection-101').show();
+ }
+ else if (dynamicContent == "1-1-0") {
+   $('#erklärungssection-110').show();
+ }
+ else if (dynamicContent == "1-1-1") {
+   $('#erklärungssection-111').show();
+ }
+ // Check if the URL parmeter is empty or not defined, display default content
+ else {
+   $('#erklärungssection-000').show();
+ }
+
+ // alert(dynamicLink);
+ alert(dynamicContent);
+
+ $("#backToSurveyLink").attr("href", dynamicLink);
+
+});
---
+++ js/setDynamicContent.js
@@ -0,0 +1,23 @@
+// display dynamic content based on Link
+// Parse the URL parameter
+
+	function getParameterByName(name, url) {
+	    if (!url) url = window.location.href;
+	    name = name.replace(/[\[\]]/g, "\\$&");
+	    var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+	        results = regex.exec(url);
+	    if (!results) return null;
+	    if (!results[2]) return '';
+	    return decodeURIComponent(results[2].replace(/\+/g, " "));
+	}
+
+	// clears local storage
+	window.localStorage.clear();
+
+	// Give the parameter a variable name
+	var dynamicContent = getParameterByName('grp');
+
+	// save new defined parameter variable to local storage
+	localStorage.setItem("dynamicContent", dynamicContent);
+	
+	// localStorage.setItem(dynamicContent, getParameterByName("grp"));
